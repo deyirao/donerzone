61b5083e35f88e4d2f8d01f72b20fcd42e8cf8ce
Deyi Rao <deyi.rao@gmx.de>
2025-03-30 00:08:42 +0100

    Complete gallery and reviews section

---
+++ js/reviews.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".reviews-slider", {
+        loop: true,
+        spaceBetween: -100,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        autoplay: {
+            delay: 7000,
+            disableOnInteraction: true,
+        },
+        breakpoints: {
+            1024: {
+                slidesPerView: 3
+            },
+            768: {
+                slidesPerView: 2
+            },
+            480: {
+                slidesPerView: 1
+            },
+        },
+    });
+});
